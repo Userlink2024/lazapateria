a779c8dc99158e365f2191392c7e995381013aab
Userlink2024 <jhoanc_felipe@hotmail.com>
2025-01-13 10:15:17 -0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,12 +1,12 @@
 // firebaseConfig.js
 const firebaseConfig = {
-    apiKey: "AIzaSyCwGpyvoUfbXE4rpPfLmHSdOK_VI4mT24M",
-    authDomain: "lazapateria-ce24f.firebaseapp.com",
-    projectId: "lazapateria-ce24f",
-    storageBucket: "lazapateria-ce24f.firebasestorage.app",
-    messagingSenderId: "642571845821",
-    appId: "1:642571845821:web:45858a0abc0df728fd77d4",
-    measurementId: "G-6JCF33H0K1"
+  apiKey: "AIzaSyChvezwN6DrYXdnBvGLxnjhi4I9xUaxKfU",
+  authDomain: "lazapateria-c4157.firebaseapp.com",
+  projectId: "lazapateria-c4157",
+  storageBucket: "lazapateria-c4157.firebasestorage.app",
+  messagingSenderId: "300241769138",
+  appId: "1:300241769138:web:8ca1847de3c4fb5042afab",
+  measurementId: "G-HVG7615JEE"
 };
 
 // Inicializar Firebase
